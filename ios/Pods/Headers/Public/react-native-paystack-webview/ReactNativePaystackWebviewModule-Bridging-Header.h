b6385b92e123b59033f8ecd61222ed6a4519427a
Siyam Rahman <siyam@Siyams-MacBook-Pro.local>
2022-02-03 22:12:14 +0600

    some

---
+++ ios/Pods/Headers/Public/react-native-paystack-webview/ReactNativePaystackWebviewModule-Bridging-Header.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-paystack-webview/ios/ReactNativePaystackWebviewModule-Bridging-Header.h
---
+++ ios/Pods/Target Support Files/react-native-paystack-webview/react-native-paystack-webview-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_react_native_paystack_webview : NSObject
+@end
+@implementation PodsDummy_react_native_paystack_webview
+@end
